dbe14fa10f15547bf8b829e976381a7cf27f00fd
xMurdererr <62215220+xMurdererr@users.noreply.github.com>
2023-11-15 00:20:31 +0000

    Add Screen class and KeyboardInterrupt exception

--- screensaver.py
+++ screensaver.py
@@ -1,79 +1,91 @@
 import time
 
-
 COLUMNS = 40
 ROWS = 15
-DELAY = 0.05
+DELAY = .1
 vector = [1, -1]
 position = [0, 0]
 
 
-class bcolors:
+class Bcolors:
     BLUE = '\033[94m'
     RED = '\033[91m'
     YELLOW = '\033[93m'
     END = '\033[0m'
 
 
-class Data:
-    BALL = f'{bcolors.RED}X{bcolors.END}'
+class Ball:
+    BALL = f'{Bcolors.RED}X{Bcolors.END}'
     WIDTH = COLUMNS - 1
     LENGTH = ROWS - 1
 
+class Screen:
+    def __init__(self, screen, vector):
+        self.screen = screen
+        self.vector = vector
 
-def draw(screen):
-    print('\n')
-    for row in screen:
-        print('  '.join(row))
+    def draw(self):
+        print('\n')
+        for row in screen:
+            print('  '.join(row))
+
+    def update_vector(self):
+        if position[0] == Ball.WIDTH:
+            if position[1] == 0:
+                self.vector = [1, -1]
+            elif position[1] == Ball.LENGTH:
+                self.vector = [-1, -1]
+            else:
+                if self.vector == [-1, 1]:
+                    self.vector = [-1, -1]
+                elif self.vector == [1, 1]:
+                    self.vector = [1, -1]
+
+        elif position[0] == 0:
+            if position[1] == 0:
+                self.vector = [1, 1]
+            elif position[1] == Ball.LENGTH:
+                self.vector = [-1, 1]
+            else:
+                if self.vector == [-1, -1]:
+                    self.vector = [-1, 1]
+                elif self.vector == [1, -1]:
+                    self.vector = [1, 1]
+
+        elif position[1] == 0:
+            if self.vector == [-1, 1]:
+                self.vector = [1, 1]
+            elif self.vector == [-1, -1]:
+                self.vector = [1, -1]
+
+        elif position[1] == Ball.LENGTH:
+            if self.vector == [1, 1]:
+                self.vector = [-1, 1]
+            elif self.vector == [1, -1]:
+                self.vector = [-1, -1]
+
+        return self.vector
 
 
-screen = [
-    [f'{bcolors.BLUE}*{bcolors.END}' for _ in range(COLUMNS)] for _ in range(ROWS)]
-screen[position[1]][position[0]] = Data.BALL
+screen = [[f'{Bcolors.BLUE}*{Bcolors.END}' for _ in range(COLUMNS)] for _ in range(ROWS)]
+board = Screen(screen, vector)
 
-draw(screen)
+board.screen[position[1]][position[0]] = Ball.BALL
 
-while True:
+board.draw()
 
-    if position[0] == Data.WIDTH:
-        if position[1] == 0:
-            vector = [1, -1]
-        elif position[1] == Data.LENGTH:
-            vector = [-1, -1]
-        else:
-            if vector == [-1, 1]:
-                vector = [-1, -1]
-            elif vector == [1, 1]:
-                vector = [1, -1]
+try:
+    while True:
 
-    elif position[0] == 0:
-        if position[1] == 0:
-            vector = [1, 1]
-        elif position[1] == Data.LENGTH:
-            vector = [-1, 1]
-        else:
-            if vector == [-1, -1]:
-                vector = [-1, 1]
-            elif vector == [1, -1]:
-                vector = [1, 1]
+        vector = board.update_vector()
 
-    elif position[1] == 0:
-        if vector == [-1, 1]:
-            vector = [1, 1]
-        elif vector == [-1, -1]:
-            vector = [1, -1]
+        screen[position[1]][position[0]] = f'{Bcolors.YELLOW}*{Bcolors.END}'
+        position[1] += vector[0]
+        position[0] += vector[1]
+        screen[position[1]][position[0]] = Ball.BALL
 
-    elif position[1] == Data.LENGTH:
-        if vector == [1, 1]:
-            vector = [-1, 1]
-        elif vector == [1, -1]:
-            vector = [-1, -1]
+        board.draw()
 
-    screen[position[1]][position[0]] = f'{bcolors.YELLOW}*{bcolors.END}'
-    position[1] += vector[0]
-    position[0] += vector[1]
-    screen[position[1]][position[0]] = Data.BALL
-
-    draw(screen)
-
-    time.sleep(DELAY)
+        time.sleep(DELAY)
+except KeyboardInterrupt:
+    print('Ended.')
